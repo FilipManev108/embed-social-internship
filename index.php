7eca3d59971c477a21ea22ae1346605902ea41f9
Filip Manev <7605817-FilipManev@users.noreply.gitlab.com>
2021-08-26 20:34:57 +0200

    task complete, added full comments for better readability

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <?php
 session_start();
 
+require_once 'functions.php'; //access to the functions.php file
+
+//writing the full index.php URL into session to be used for redirecting back from logic.php
+$_SESSION['linkURL'] = linkURL(); //function explained in detail in functions.php on line 153
+
+//default values for dropdown options on first load
 $rating = 0;
 $rating_filter = 1;
 $date = 0;
 $text = 0;
 
+//arrays used in the construction of the dropdown option forms
 $binary_choice = [1, 0];
 $rating_choice = [1, 2, 3, 4, 5];
 
+//empty array for later handling of the sorted data received from the logic.php file
 $new_json_data = [];
 
 if (isset($_SESSION['json'])) {
-    // echo 'SESSION IS OK';
+
+    //rewriting the defailt values for the dropdowns - used for remembering the old selected values on window reload/form submission
     $rating = $_SESSION['rating'];
     $rating_filter = $_SESSION['rating-filter'];
     $date = $_SESSION['date'];
     $text = $_SESSION['text'];
 
+    //receiveing the fully sorted json data array from logic.php file
     $new_json_data = $_SESSION['json'];
 }
-$c = 0;
 
 ?>
 
@@ -64,6 +73,7 @@ $c = 0;
             <label for="rating">Order by rating:</label>
             <select name="rating" id="rating">
 
+                <!-- loop for rating dropdown options construction -->
                 <?php foreach ($binary_choice as $choice) { ?>
 
                     <option value="<?= $choice ?>" <?php if ($choice == $rating) { ?> selected <?php } ?>><?= ($choice) ? "Highest first" : "Lowest First" ?></option>
@@ -78,6 +88,7 @@ $c = 0;
             <label for="rating-filter">Minimum rating:</label>
             <select name="rating-filter" id="rating-filter">
 
+                <!-- loop for rating-filter dropdown options construction -->
                 <?php foreach ($rating_choice as $choice) { ?>
 
                     <option value="<?= $choice ?>" <?php if ($choice == $rating_filter) { ?> selected <?php } ?>><?= $choice ?></option>
@@ -92,6 +103,7 @@ $c = 0;
             <label for="date">Order by date:</label>
             <select name="date" id="date">
 
+                <!-- loop for date dropdown options construction -->
                 <?php foreach ($binary_choice as $choice) { ?>
 
                     <option value="<?= $choice ?>" <?php if ($choice == $date) { ?> selected <?php } ?>><?= ($choice) ? "Newest first" : "Oldest First" ?></option>
@@ -106,6 +118,7 @@ $c = 0;
             <label for="text">Prioritize by text:</label>
             <select name="text" id="text">
 
+                <!-- loop for text dropdown options construction -->
                 <?php foreach ($binary_choice as $choice) { ?>
 
                     <option value="<?= $choice ?>" <?php if ($choice == $text) { ?> selected <?php } ?>><?= ($choice) ? "Yes" : "No" ?></option>
@@ -135,13 +148,17 @@ $c = 0;
 
         <!-- table body -->
         <tbody>
+
+            <!-- looping through the fully sorted new json data array -->
             <?php foreach ($new_json_data as $value) { ?>
+
                 <tr>
                     <td><?= $value['id'] ?></td>
                     <td><?= $value['rating'] ?></td>
                     <td><?= $value['reviewCreatedOnDate'] ?></td>
                     <td><?= ($value['reviewText'] == '') ? 'no text' : $value['reviewText'] ?></td>
                 </tr>
+
             <?php } ?>
         </tbody>
     </table>
--- logic.php
+++ logic.php
@@ -1,75 +1,84 @@
 <?php
 session_start();
-require_once 'functions.php';
 
-// print_r($_POST);
-// echo "<hr>";
+require_once 'functions.php'; //access to the functions.php file
+
+//writing the full URL from index.php for redirecting back after all the operations are done
+$link_index = $_SESSION['linkURL'];
+
+//assigning the $_POST values passed from index.php to $_SESSION variables - to be used back in index.php for functionality of the dropdown form inputs
 $_SESSION['rating'] = $_POST['rating'];
 $_SESSION['rating-filter'] = $_POST['rating-filter'];
 $_SESSION['date'] = $_POST['date'];
 $_SESSION['text'] = $_POST['text'];
-$json = file_get_contents('data.json');
 
-$json_data = json_decode($json, true);
-$new_json_data = [];
+$json = file_get_contents('data.json'); //getting the data from the data.json file
 
+$json_data = json_decode($json, true); //transforming the json format into a PHP associative array
 
-// minimum rating
+$new_json_data = []; //empty array in which all the magic happens later on
+
+//looping through the main, unfiletered json data array - if the configured minimum rating is less than 5, the working array is shortened thus contributing to optimization
 for ($i = 0; $i < count($json_data); $i++) {
+
+    //conditional for filtering the configured minimum rating elements, as selected in the index.php form
     if ($json_data[$i]['rating'] >= $_POST['rating-filter']) {
+
+        //adding the filtered elements into a new json data array
         $new_json_data[] = $json_data[$i];
     }
 }
-// echo '<pre>';
-// print_r(sortDate($new_json_data, 'ASC'));
-if ($_POST['date'] == 0) {
-    $new_json_data = sortDate($new_json_data, 'ASC');
-    // print_r($new_json_data);
-}
+
+//conditional for sorting the new json data array by descending date order
 if ($_POST['date'] == 1) {
-    $new_json_data = sortDate($new_json_data, 'DESC');
-    // print_r($new_json_data);
+    $new_json_data = sortDate($new_json_data, 'DESC');  //this function is explained in detail in the functions.php file starting on line 3
 }
-// echo '<pre>';
-// print_r($new_json_data);
-// die();
-// print_r($new_json_data);
+
+//conditional for sorting the new json data array by ascending date order
+if ($_POST['date'] == 0) {
+    $new_json_data = sortDate($new_json_data, 'ASC'); //this function is explained in detail in the functions.php file starting on line 3 
+}
+
+//conditional for sorting the new json data array by ascending rating order
 if ($_POST['rating'] == 1) {
+
     //no text division
     if ($_POST['text'] == 0) {
-        // $new_json_data = [];
-        $new_json_data = sortRating($new_json_data, 'DESC');
-        $_SESSION['json'] = $new_json_data;
+        $new_json_data = sortRating($new_json_data, 'DESC'); //function explained in detail in functions.php on line 47  
+        $_SESSION['json'] = $new_json_data; //passing the fully sorted new json data array in session, used in the construction of the table body
     }
+
     //text division
     if ($_POST['text'] == 1) {
-        $text_json_data = textOnly($new_json_data);
-        $no_text_json_data = noTextOnly($new_json_data);
-        $sorted_rating_with_text = sortRating($text_json_data, 'DESC');
-        $sorted_rating_without_text = sortRating($no_text_json_data, 'DESC');
-        $new_json_data = [];
-        $new_json_data = sortedTextAndRating($sorted_rating_with_text, $sorted_rating_without_text);
-        $_SESSION['json'] = $new_json_data;
+        $text_json_data = textOnly($new_json_data); //function explained in detail in functions.php on line 91  
+        $no_text_json_data = noTextOnly($new_json_data); //function explained in detail in functions.php on line 111  
+        $sorted_rating_with_text = sortRating($text_json_data, 'DESC'); //function explained in detail in functions.php on line 47  
+        $sorted_rating_without_text = sortRating($no_text_json_data, 'DESC'); //function explained in detail in functions.php on line 47  
+        $new_json_data = sortedTextAndRating($sorted_rating_with_text, $sorted_rating_without_text); //function explained in detail in functions.php on line 131  
+        $_SESSION['json'] = $new_json_data; //passing the fully sorted new json data array in session, used in the construction of the table body
     }
 }
+
+//conditional for sorting the new json data array by ascending rating order
 if ($_POST['rating'] == 0) {
+
+    //no text division
     if ($_POST['text'] == 0) {
-        // $new_json_data = [];
-        $new_json_data = sortRating($new_json_data, 'ASC');
-        $_SESSION['json'] = $new_json_data;
+        $new_json_data = sortRating($new_json_data, 'ASC'); //function explained in detail in functions.php on line 47  
+        $_SESSION['json'] = $new_json_data; //passing the fully sorted new json data array in session, used in the construction of the table body
     }
+
     //text division
     if ($_POST['text'] == 1) {
-        $text_json_data = textOnly($new_json_data);
-        $no_text_json_data = noTextOnly($new_json_data);
-        $sorted_rating_with_text = sortRating($text_json_data, 'ASC');
-        $sorted_rating_without_text = sortRating($no_text_json_data, 'ASC');
-        $new_json_data = [];
-        $new_json_data = sortedTextAndRating($sorted_rating_with_text, $sorted_rating_without_text);
-        $_SESSION['json'] = $new_json_data;
+        $text_json_data = textOnly($new_json_data); //function explained in detail in functions.php on line 91 
+        $no_text_json_data = noTextOnly($new_json_data); //function explained in detail in functions.php on line 111  
+        $sorted_rating_with_text = sortRating($text_json_data, 'ASC'); //function explained in detail in functions.php on line 47  
+        $sorted_rating_without_text = sortRating($no_text_json_data, 'ASC'); //function explained in detail in functions.php on line 47  
+        $new_json_data = sortedTextAndRating($sorted_rating_with_text, $sorted_rating_without_text); //function explained in detail in functions.php on line 131  
+        $_SESSION['json'] = $new_json_data; //passing the fully sorted new json data array in session, used in the construction of the table body
     }
 }
 
 
-
-header("Location: http://localhost/job%20hunt/embed-social-internship/");
+//redirecting back to index.php  
+header('Location: ' . $link_index);
